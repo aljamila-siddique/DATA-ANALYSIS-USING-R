99c82593a389e3b9504c9dd7d26502aff4619b86
aljamila-siddique <aljameelasidhique@gmail.com>
2021-05-19 06:41:36 +0000

    cleaned and arranged

--- covid19dataanalysisUS.r
+++ covid19dataanalysisUS.r
@@ -9,3 +9,23 @@ tsc=tsc%>%filter(Country.Region=='US')
 tsc
 
 tsc=data.frame(t(tsc))
+#Changing date,index to variable
+tsc= cbind(rownames(tsc), data.frame(tsc, row.names=NULL))
+
+
+#Giving Column Names
+
+colnames(tsc)=c('Date','Confirmed')
+tsc=tsc[-c(1:4),]
+tsc
+
+summary(tsc)
+
+#converting date tonumeric
+
+tsc$Date=ymd((tsc$Date))
+
+tsc$Date =ymd((tsc$Date))
+tsc$Confirmed=as.numeric(tsc$Confirmed)
+
+qplot(Date,Confirmed,data = tsc,main='Covid 19 Confirmed Cases in USA')             
